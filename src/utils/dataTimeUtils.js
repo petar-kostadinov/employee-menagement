ec3655486ae0f3dafb6bbc00d4703695d6099069
petar-kostadinov <153288029+petar-kostadinov@users.noreply.github.com>
2026-09-24 21:10:17 +0300

    fix date and phoneNumber

---
+++ src/utils/dataTimeUtils.js
@@ -0,0 +1,14 @@
+export const fromIsoDate = (isoDate) => {
+  const date = new Date(isoDate);
+
+  const formattedDate = date.toLocaleString(
+    'en-US',
+    {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    },
+  );
+
+  return formattedDate;
+};
